b333d29a59d0efce753c1d2b6b791a34152a5f5a
Rhodinium <103146200+Karxii@users.noreply.github.com>
2022-06-05 19:00:26 +0100

    Final

---
+++ server-data/resources/[stream]/[maps]/sound/__resource.lua
@@ -0,0 +1,28 @@
+resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
+
+files {
+  'audio/sfx/weapons_player/lmg_combat.awc',
+  'audio/sfx/weapons_player/lmg_mg_player.awc',
+  'audio/sfx/weapons_player/mgn_sml_am83_vera.awc',
+  'audio/sfx/weapons_player/mgn_sml_am83_verb.awc',
+  'audio/sfx/weapons_player/mgn_sml_sc__l.awc',
+  'audio/sfx/weapons_player/ptl_50cal.awc',
+  'audio/sfx/weapons_player/ptl_combat.awc',
+  'audio/sfx/weapons_player/ptl_pistol.awc',
+  'audio/sfx/weapons_player/ptl_px4.awc',
+  'audio/sfx/weapons_player/ptl_rubber.awc',
+  'audio/sfx/weapons_player/sht_bullpup.awc',
+  'audio/sfx/weapons_player/sht_pump.awc',
+  'audio/sfx/weapons_player/smg_micro.awc',
+  'audio/sfx/weapons_player/smg_smg.awc',
+  'audio/sfx/weapons_player/snp_heavy.awc',
+  'audio/sfx/weapons_player/snp_rifle.awc',
+  'audio/sfx/weapons_player/spl_grenade_player.awc',
+  'audio/sfx/weapons_player/spl_minigun_player.awc',
+  'audio/sfx/weapons_player/spl_prog_ar_player.awc',
+  'audio/sfx/weapons_player/spl_railgun.awc',
+  'audio/sfx/weapons_player/spl_rpg_player.awc',
+  'audio/sfx/weapons_player/spl_tank_player.awc',
+}
+
+data_file 'AUDIO_WAVEPACK' 'audio/sfx/weapons_player'
