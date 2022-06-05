c6985dfebe7eed6d5ba47c11bfacc5e8eed0a155
Rhodinium <103146200+Karxii@users.noreply.github.com>
2022-06-05 18:33:49 +0100

    Maps Push One

---
+++ server-data/resources/[stream]/[maps]/[casino]/sup-casinomisc/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Gabz and Xee smile'
+description 'Supremacy Casino'
+version '1.0.0'
+
+this_is_a_map 'yes'
+
+files{
+    "peds.meta",
+}
+
+data_file "PED_METADATA_FILE" "peds.meta"
