8bff0ab85fb912128d529bdc881744969df52cb5
Rhodinium <103146200+Karxii@users.noreply.github.com>
2022-06-05 18:37:30 +0100

    Maps Push Two

---
+++ server-data/resources/[stream]/[maps]/[int-mlos]/int_vanillaunicorn/fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'bodacious'
+games { 'gta5' }
+
+author 'Gabz'
+description 'Vanilla Unicorn'
+version '1.0.0'
+
+this_is_a_map 'yes'
---
+++ server-data/resources/[stream]/[maps]/[new-mlo]/Saloon/__resource.lua
@@ -0,0 +1,12 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627fdkf541'
+
+this_is_a_map 'yes'
+
+file 'gusepe_timecycle_mods_saloon.xml'
+data_file 'TIMECYCLEMOD_FILE' 'gusepe_timecycle_mods_saloon.xml'
+data_file 'DLC_ITYP_REQUEST' 'stream/int_retail.ytyp' 
+data_file 'DLC_ITYP_REQUEST' 'stream/vw_prop_vw_tables.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/ch_prop_ch_casino_accs_02.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/int_industrial.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/v_int_10.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/ch_dlc_int_02_ch.ytyp'
